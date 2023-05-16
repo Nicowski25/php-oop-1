6f3b076e4d46d7f993c8a052fdee74fe8c73ed20
Nicowski25 <nicola.faedo0610@gmail.com>
2023-05-16 17:13:50 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+class Movie {
+    public $title;
+    public $description;
+    public $director;
+    public $genre;
+    public $year;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Object Oriented</title>
+</head>
+<body>
+    
+</body>
+</html>
